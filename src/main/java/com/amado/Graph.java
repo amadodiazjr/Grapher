1cceb58300a844b304e05753cf93a6559d788ab9
amadodiazjr <amado.diaz.jr@gmail.com>
2014-10-26 00:01:56 -0400

    completed graph test

--- src/main/java/com/amado/Graph.java
+++ src/main/java/com/amado/Graph.java
@@ -1,5 +1,11 @@
 package com.amado;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 import org.apache.commons.lang3.Validate;
 
 public class Graph {
@@ -10,5 +16,40 @@ public class Graph {
 		_root = root;
 	}
 	
-	
+	public List<GraphNode> findPath(final GraphNode node) {
+		final Set<Integer> visited = new HashSet<Integer>();		
+		final Stack<GraphNode> stack = new Stack<GraphNode>();
+		stack.push(_root);
+
+		while (!stack.isEmpty()) {
+			final GraphNode top = stack.peek();
+			final Integer key = top.getKey();			
+			visited.add(key);
+			if (key.equals(node.getKey())) {
+				return stack;
+			}
+			
+			final Map<Integer, GraphNode> graphNodes = top.getGraphNodes();
+			if (graphNodes.isEmpty()) {
+				stack.pop();
+				continue;
+			}
+			
+			final Set<Integer> keySet = graphNodes.keySet();
+			Boolean hasUnvisitedNode = Boolean.FALSE;
+			for (final Integer integer : keySet) {
+				hasUnvisitedNode = !visited.contains(integer);
+				if (hasUnvisitedNode) {
+					stack.push(graphNodes.get(integer));
+					break;
+				}
+			}
+			
+			if (!hasUnvisitedNode) {
+				stack.pop();	
+			}
+		}
+		
+		return stack;
+	}
 }
--- src/test/java/com/amado/unit/GraphTest.java
+++ src/test/java/com/amado/unit/GraphTest.java
@@ -4,6 +4,8 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.amado.Graph;
@@ -23,4 +25,41 @@ public class GraphTest {
 		assertThat(graph, is(notNullValue()));
 	}
 
+	@Test
+	public void canFindPath() {
+		// ~given
+		final GraphNode one = new GraphNode(1);
+		final GraphNode two = new GraphNode(2);
+		final GraphNode four = new GraphNode(4);
+		one.linkGraphNode(two);
+		one.linkGraphNode(four);
+
+		final GraphNode three = new GraphNode(3);
+		final GraphNode five = new GraphNode(5);
+		two.linkGraphNode(three);
+		two.linkGraphNode(five);
+
+		final GraphNode seven = new GraphNode(7);
+		four.linkGraphNode(seven);
+		
+		final GraphNode six = new GraphNode(6);
+		five.linkGraphNode(six);
+		
+		final GraphNode nine = new GraphNode(9);
+		six.linkGraphNode(nine);
+		
+		final GraphNode eight = new GraphNode(8);
+		nine.linkGraphNode(eight);
+		
+		final Graph graph = new Graph(one);
+		
+		// ~when
+		List<GraphNode> path = graph.findPath(six);
+		
+		// ~then
+		assertThat(path.get(0).getKey(), is(1));
+		assertThat(path.get(1).getKey(), is(2));
+		assertThat(path.get(2).getKey(), is(5));
+		assertThat(path.get(3).getKey(), is(6));
+	}
 }
